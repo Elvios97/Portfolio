d3f1c790fb8a00249149da3a3498c2adced9fdb6
Mirco Schrammeck <Schrammeck97@gmail.com>
2026-05-30 14:00:20 +0200

    Fixed view

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Portfolio from "../portfolio_i18n.jsx";
+import Portfolio from "./portfolio_i18n.jsx";  // ./ statt ../
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
